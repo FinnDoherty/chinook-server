ee064037b800e99be5818aa16d448ba91bba23ef
Finn Doherty <15265859+FinnDoherty@users.noreply.github.com>
2021-03-13 15:11:27 +0000

    Added dao.js to access an existing database and read, write, delete from it

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express')
+const AppDAO = require('./dao')
+
 const app = express()
 const port = 3000
+const databaseLocation = './database.sqlite3'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+function main() {
+  const dao = new AppDAO(databaseLocation)
+  // the db has already been populated outside the app.
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  // check database access: read, write, delete
+  dao.all('SELECT * from Artist limit 5')
+  .then((data) => {
+    console.log(data);
+  })
+
+  dao.run('INSERT INTO Artist (Name) VALUES ("My New Band")')
+  .then((data) => {
+    console.log('new id is: ' + data.id);
+
+    dao.get('SELECT * from Artist WHERE ArtistId = ?', [data.id])
+    .then((data) => {
+      console.log(data);
+    })
+  })
+
+  dao.run('DELETE from Artist where Name = "My New Band"')
+  .then((id) => {
+    console.log(id);
+
+    console.log('deleted');
+  })
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+}
+
+main()
