0c81f0827a175f4e13621f363b88db64d5b47e62
Finn Doherty <15265859+FinnDoherty@users.noreply.github.com>
2021-03-18 00:26:37 +0000

    Added simple endpoints for artists, albums, playlists and tracks. Implemented playlists and playlist by id endpoints

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ create and populate the chinook database included:
 ## DB access server
 to run the server `npm start`
 and navigate to localhost:3000 in a browser
+
+### Endpoints
+  READ endpoints
+  TODO          /artists
+  TODO          /artists/:id
+  TODO          /albums
+  TODO          /albums/:id
+  TODO          /tracks
+  TODO          /tracks/:id
+
+  /playlists - a list of all the saved playlists, and number of tracks on each
+  /playlists/:id - playlist info, name, number of tracks, list of tracks with names, albums and artists
--- index.js
+++ index.js
@@ -7,33 +7,92 @@ const databaseLocation = './database.sqlite3'
 
 function main() {
   const dao = new AppDAO(databaseLocation)
-  // the db has already been populated outside the app.
-
-  // check database access: read, write, delete
-  dao.all('SELECT * from Artist limit 5')
-  .then((data) => {
-    console.log(data);
-  })
-
-  dao.run('INSERT INTO Artist (Name) VALUES ("My New Band")')
-  .then((data) => {
-    console.log('new id is: ' + data.id);
-
-    dao.get('SELECT * from Artist WHERE ArtistId = ?', [data.id])
-    .then((data) => {
-      console.log(data);
-    })
-  })
-
-  dao.run('DELETE from Artist where Name = "My New Band"')
-  .then((id) => {
-    console.log(id);
-
-    console.log('deleted');
-  })
 
   app.get('/', (req, res) => {
     res.send('Hello World!')
+  })
+
+  app.get('/artists', (req, res) => {
+    dao.all('SELECT * from Artist')
+    .then((data) => {
+      res.send(data);
+    })
+  })
+
+  app.get('/artists/:id', (req, res) => {
+    const { id } = req.params;
+    dao.all(`SELECT * from Artist WHERE ArtistId = ${id}`)
+    .then((data) => {
+      res.send(data);
+    })
+  })
+
+  app.get('/albums', (req, res) => {
+    dao.all('SELECT * from Album')
+    .then((data) => {
+      res.send(data);
+    })
+  })
+
+  app.get('/albums/:id', (req, res) => {
+    const { id } = req.params;
+    dao.all(`SELECT * from Album WHERE AlbumId = ${id}`)
+    .then((data) => {
+      res.send(data);
+    })
+  })
+
+
+  app.get('/playlists', (req, res) => {
+    dao.all(`
+      SELECT p.PlaylistId, p.Name, COUNT(pt.TrackId) as 'NumberOfTracks'
+      FROM Playlist p
+      LEFT JOIN PlaylistTrack pt ON p.PlaylistId = pt.PlaylistId
+      GROUP BY p.PlaylistId;
+    `)
+    .then((data) => {
+      res.send(data);
+    })
+  })
+
+  app.get('/playlists/:id', (req, res) => {
+    const { id } = req.params;
+    dao.all(`
+      SELECT p.Name, pt.TrackId, t.Name as 'Track', alb.Title as 'Album', art.Name as 'Artist',
+          COUNT(pt.TrackId) OVER (PARTITION BY pt.PlaylistId) AS 'NumberOfTracks'
+      FROM Playlist p
+      LEFT JOIN PlaylistTrack pt ON p.PlaylistId = pt.PlaylistId
+      LEFT JOIN Track t ON pt.TrackId = t.TrackId
+      LEFT JOIN Album alb ON alb.AlbumId = t.AlbumId
+      LEFT JOIN Artist art ON art.ArtistId = alb.ArtistId
+      WHERE p.PlaylistId = ${id};`
+    )
+    .then((data) => {
+      var playlistName = data[0].Name;
+      var numberOfTracks = data[0].NumberOfTracks;
+      var tracks = numberOfTracks > 0 ? data : [];
+
+      res.send({
+        playlistName: playlistName,
+        numberOfTracks: numberOfTracks,
+        tracks: tracks
+      });
+    })
+  })
+
+  app.get('/tracks', (req, res) => {
+    dao.all('SELECT * from Track')
+    .then((data) => {
+      res.send(data);
+    })
+  })
+
+  app.get('/tracks/:id', (req, res) => {
+    const { id } = req.params;
+    dao.all(`SELECT * from Track WHERE TrackId = ${id}`)
+    .then((data) => {
+      res.send(data);
+    })
   })
 
   app.listen(port, () => {
